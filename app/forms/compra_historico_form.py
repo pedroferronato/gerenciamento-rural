44c413298a4aa73dc5865b82f075adfc827b21d6
pedroferronato <pedroluisfbar@gmail.com>
2021-10-27 07:24:27 -0400

    Conversão de select para datalist

--- app/forms/compra_historico_form.py
+++ app/forms/compra_historico_form.py
@@ -4,6 +4,5 @@ from wtforms.validators import DataRequired, Length
 
 
 class CompraHistoricoForm(FlaskForm):
-    fornecedor = SelectField('Fornecedor:')
     data_inicio = DateField('Data inicial:', format='%d/%m/%Y')
     data_final = DateField('Data final:', format='%d/%m/%Y')
